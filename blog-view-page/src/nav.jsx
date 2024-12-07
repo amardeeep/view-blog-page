c60ac74531dce4a12b1a1737728697fd79253560
Deetsy <amardeepdeepak2016@gmail.com>
2024-12-07 23:11:41 +0530

    install styled-components and use it

--- blog-view-page/src/nav.jsx
+++ blog-view-page/src/nav.jsx
@@ -1,19 +1,40 @@
+import style from 'styled-components'
 function Nav(){
+    const NavBar=style.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    align-items:center;
+    padding:5px 20px;
+    `
+    const LeftNavSection=style.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    gap:20px`
+    const RightNavSection=style.div`
+     display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    gap:20px
+    `
     return(
         <>
         <nav>
         <h1>Bloggsy</h1>
-        <div className="navBar">
-            <div className="leftNavSection">
+        <NavBar>
+            <LeftNavSection>
                 <a href="">Blogs</a>
                 <a href="">About</a>
-            </div>
-            <div className="rightNavSection">
+            </LeftNavSection>
+            <RightNavSection>
                 <button>Toggle Theme</button>
                 <a href="">Login</a>
                 <a href="">Signup</a>
-            </div>
-        </div>
+            </RightNavSection>
+        </NavBar>
         </nav>
         </>
     )
